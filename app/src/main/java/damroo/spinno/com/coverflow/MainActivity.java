15ed15a02f7a683b83d2afae658bf0ea3cdfa930
Samir Goel <samirgoel3@gmail.com>
2015-09-12 17:49:53 +0530

    Initial commit

---
+++ app/src/main/java/damroo/spinno/com/coverflow/MainActivity.java
@@ -0,0 +1,78 @@
+package damroo.spinno.com.coverflow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+
+import it.moondroid.coverflow.components.ui.containers.FeatureCoverFlow;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FeatureCoverFlow mCoverFlow;
+    private CoverFlowAdapter mAdapter;
+    private ArrayList<GameEntity> mData = new ArrayList<>(0);
+    private TextSwitcher mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coverflow);
+
+        mData.add(new GameEntity(R.drawable.image_1, R.string.title1));
+        mData.add(new GameEntity(R.drawable.image_2, R.string.title2));
+        mData.add(new GameEntity(R.drawable.image_3, R.string.title3));
+        mData.add(new GameEntity(R.drawable.image_4, R.string.title4));
+
+        mTitle = (TextSwitcher) findViewById(R.id.title);
+        mTitle.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                TextView textView = (TextView) inflater.inflate(R.layout.item_title, null);
+                return textView;
+            }
+        });
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.slide_in_top);
+        Animation out = AnimationUtils.loadAnimation(this, R.anim.slide_out_bottom);
+        mTitle.setInAnimation(in);
+        mTitle.setOutAnimation(out);
+
+        mAdapter = new CoverFlowAdapter(this);
+        mAdapter.setData(mData);
+        mCoverFlow = (FeatureCoverFlow) findViewById(R.id.coverflow);
+        mCoverFlow.setAdapter(mAdapter);
+
+        mCoverFlow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,
+                        getResources().getString(mData.get(position).titleResId),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mCoverFlow.setOnScrollPositionListener(new FeatureCoverFlow.OnScrollPositionListener() {
+            @Override
+            public void onScrolledToPosition(int position) {
+                mTitle.setText(getResources().getString(mData.get(position).titleResId));
+            }
+
+            @Override
+            public void onScrolling() {
+                mTitle.setText("");
+            }
+        });
+
+    }
+
+}
